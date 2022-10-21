aa4c08e276896a534bb734456401ae213727f80e
Patryk <patrykjanas27@gmail.com>
2022-10-21 17:00:40 +0200

    Refactor

--- src/main/java/org/example/lab2/App.java
+++ src/main/java/org/example/lab2/App.java
@@ -10,14 +10,22 @@ public class App {
         Function<Double, Double> uniFunction = x -> 2 * pow(x, 2) + 3 * x - 8;
         BiFunction<Double, Double, Double> biFunction = (x, y) -> -5 * pow(x, 2) * y + 2 * x * pow(y, 2) + 10;
 
+        showResult(uniFunction);
+        showResult(biFunction);
+
+    }
+
+    private static void showResult(Function<Double, Double> uniFunction) {
         System.out.println("Uni Result for n = 2: " + IntegrateService.calculate(uniFunction, 2));
         System.out.println("Uni Result for n = 3: " + IntegrateService.calculate(uniFunction, 3));
         System.out.println("Uni Result for n = 4: " + IntegrateService.calculate(uniFunction, 4));
         System.out.println("Uni Result for n = 5: " + IntegrateService.calculate(uniFunction, 5));
+    }
+
+    private static void showResult(BiFunction<Double, Double, Double> biFunction) {
         System.out.println("Bi Result for n = 2: " + IntegrateService.calculate(biFunction, 2));
         System.out.println("Bi Result for n = 3: " + IntegrateService.calculate(biFunction, 3));
         System.out.println("Bi Result for n = 4: " + IntegrateService.calculate(biFunction, 4));
         System.out.println("Bi Result for n = 5: " + IntegrateService.calculate(biFunction, 5));
-
     }
 }
