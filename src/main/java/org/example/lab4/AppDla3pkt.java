1e058c026985daf0e773ba50bf28084a89affd51
Patryk <patrykjanas27@gmail.com>
2022-12-01 19:52:04 +0100

    refactor with mainMatrix

--- src/main/java/org/example/lab4/AppDla3pkt.java
+++ src/main/java/org/example/lab4/AppDla3pkt.java
@@ -1,39 +1,30 @@
 package org.example.lab4;
 
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Function;
-
-import static java.lang.Math.sqrt;
-
 public class AppDla3pkt {
-    public static double[][] mainMatrix = new double[2][2];
-    public static void main(String[] args) throws FileNotFoundException {
-        calculateAndShowFirstArray();
-        calculateAndShowSecondArray();
-        //===================Calculation for determinant===========
-        double determinant = DeterminantService.getDeterminant2x2(mainMatrix); // 0,00015625
-        mainMatrix = DeterminantService.getMatrix2x2MultipliedBy1DividedByDeterminant(mainMatrix, determinant);
 
-
-        double[][] matrixHForTwoPointIntegration = getMatrixHForTwoPointIntegration(determinant);
+    public static void main(String[] args) {
+        double[][] matrixHForTwoPointIntegration = getMatrixHForTwoPointIntegration();
         System.out.println("MatrixHForTwoPointIntegration: ");
         MatrixService.showTable2D(matrixHForTwoPointIntegration);
-        double[][] matrixHForThreePointIntegration = getMatrixHForThreePointIntegration(determinant);
+        double[][] matrixHForThreePointIntegration = getMatrixHForThreePointIntegration();
         System.out.println("MatrixHForThreePointIntegration: ");
         MatrixService.showTable2D(matrixHForThreePointIntegration);
-        double[][] matrixHForFourPointIntegration = getMatrixHForFourPointIntegration(determinant);
+        double[][] matrixHForFourPointIntegration = getMatrixHForFourPointIntegration();
         System.out.println("MatrixHForFourPointIntegration: ");
         MatrixService.showTable2D(matrixHForFourPointIntegration);
     }
 
-    private static double[][] getMatrixHForTwoPointIntegration(double determinant) {
+    private static double[][] getMatrixHForTwoPointIntegration() {
+        double[][] mainMatrix = MatrixService.getMainMatrix();
+        //===================Calculation for determinant===========
+        double determinant = DeterminantService.getDeterminant2x2(mainMatrix); // 0,00015625
+        mainMatrix = DeterminantService.getMatrix2x2MultipliedBy1DividedByDeterminant(mainMatrix, determinant);
+
         double[][] tableOfKsiIntegral = IntegralFunctions.calculateAndShowFirstArray();
         double[][] tableOfEtaIntegral = IntegralFunctions.calculateAndShowSecondArray();
         //===================Two point integration==========
-        double[][] tableOfKsiIntegralByM = getTableOfKsi1IntegralMultipliedByMatrix(tableOfKsiIntegral, tableOfEtaIntegral);
-        double[][] tableOfEtaIntegralByM = getTableOfEta2IntegralMultipliedByMatrix(tableOfKsiIntegral, tableOfEtaIntegral);
+        double[][] tableOfKsiIntegralByM = getTableOfKsi1IntegralMultipliedByMatrix(mainMatrix, tableOfKsiIntegral, tableOfEtaIntegral);
+        double[][] tableOfEtaIntegralByM = getTableOfEta2IntegralMultipliedByMatrix(mainMatrix, tableOfKsiIntegral, tableOfEtaIntegral);
 //        System.out.println("tableOfKsiIntegralByM 1: ");
 //        MatrixService.showTable2D(tableOfKsiIntegralByM);
 //        System.out.println("tableOfEtaIntegralByM 2:");
@@ -59,13 +50,17 @@ public class AppDla3pkt {
         return mainH;
     }
 
-    private static double[][] getMatrixHForThreePointIntegration(double determinant) {
+    private static double[][] getMatrixHForThreePointIntegration() {
+        double[][] mainMatrix = MatrixService.getMainMatrix();
+        //===================Calculation for determinant===========
+        double determinant = DeterminantService.getDeterminant2x2(mainMatrix); // 0,00015625
+        mainMatrix = DeterminantService.getMatrix2x2MultipliedBy1DividedByDeterminant(mainMatrix, determinant);
 
         double[][] tableOfKsiIntegral = IntegralFunctions.calculateAndShowThirdArray();
         double[][] tableOfEtaIntegral = IntegralFunctions.calculateAndShowFourthArray();
         //===================Three point integration==========
-        double[][] tableOfKsiIntegralByM = getTableOfKsi1IntegralMultipliedByMatrix(tableOfKsiIntegral, tableOfEtaIntegral);
-        double[][] tableOfEtaIntegralByM = getTableOfEta2IntegralMultipliedByMatrix(tableOfKsiIntegral, tableOfEtaIntegral);
+        double[][] tableOfKsiIntegralByM = getTableOfKsi1IntegralMultipliedByMatrix(mainMatrix, tableOfKsiIntegral, tableOfEtaIntegral);
+        double[][] tableOfEtaIntegralByM = getTableOfEta2IntegralMultipliedByMatrix(mainMatrix, tableOfKsiIntegral, tableOfEtaIntegral);
 //        System.out.println("tableOfKsiIntegralByM 1: ");
 //        MatrixService.showTable2D(tableOfKsiIntegralByM);
 //        System.out.println("tableOfEtaIntegralByM 2:");
@@ -101,12 +96,17 @@ public class AppDla3pkt {
         return mainH;
     }
 
-    private static double[][] getMatrixHForFourPointIntegration(double determinant) {
+    private static double[][] getMatrixHForFourPointIntegration() {
+        double[][] mainMatrix = MatrixService.getMainMatrix();
+        //===================Calculation for determinant===========
+        double determinant = DeterminantService.getDeterminant2x2(mainMatrix); // 0,00015625
+        mainMatrix = DeterminantService.getMatrix2x2MultipliedBy1DividedByDeterminant(mainMatrix, determinant);
+
         double[][] tableOfKsiIntegral = IntegralFunctions.calculateAndShowFifthArray();
         double[][] tableOfEtaIntegral = IntegralFunctions.calculateAndShowSixthArray();
         //===================Four point integration==========
-        double[][] tableOfKsiIntegralByM = getTableOfKsi1IntegralMultipliedByMatrix(tableOfKsiIntegral, tableOfEtaIntegral);
-        double[][] tableOfEtaIntegralByM = getTableOfEta2IntegralMultipliedByMatrix(tableOfKsiIntegral, tableOfEtaIntegral);
+        double[][] tableOfKsiIntegralByM = getTableOfKsi1IntegralMultipliedByMatrix(mainMatrix, tableOfKsiIntegral, tableOfEtaIntegral);
+        double[][] tableOfEtaIntegralByM = getTableOfEta2IntegralMultipliedByMatrix(mainMatrix, tableOfKsiIntegral, tableOfEtaIntegral);
 //        System.out.println("tableOfKsiIntegralByM 1: ");
 //        MatrixService.showTable2D(tableOfKsiIntegralByM);
 //        System.out.println("tableOfEtaIntegralByM 2:");
@@ -158,7 +158,6 @@ public class AppDla3pkt {
 
 
     private static double[][] calculateHpc(double[][] table1DlaPc1, double[][] table2DlaPc1, double detJ, int whichPc) {
-        //Obliczanie macierzy H dla pierwszego punktu calkowania
         double[][] table1DlaH = new double[4][4];
         double[][] table2DlaH = new double[4][4];
         for (int i = 0; i < 4; i++) {
@@ -167,24 +166,16 @@ public class AppDla3pkt {
                 table2DlaH[i][j] = table2DlaPc1[whichPc][i] * table2DlaPc1[whichPc][j];
             }
         }
-        //dV realizujemy poprzez przemnożenie wyniku przez Jakobian przekształcenia tego punktu całkowania
         double[][] Hpc = new double[4][4];
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                double value = 30 * (table1DlaH[i][j] + table2DlaH[i][j]) * detJ;
-//                value = BigDecimal.valueOf(value)
-//                        .setScale(3, RoundingMode.HALF_UP)
-//                        .doubleValue();
-                Hpc[i][j] = value;
-//                System.out.print(Hpc[i][j] + ", ");
+                Hpc[i][j] = 30 * (table1DlaH[i][j] + table2DlaH[i][j]) * detJ;
             }
-//            System.out.println();
         }
-
         return Hpc;
     }
 
-    private static double[][] getTableOfEta2IntegralMultipliedByMatrix(double[][] tableOfKsiIntegral, double[][] tableOfEtaIntegral) {
+    private static double[][] getTableOfEta2IntegralMultipliedByMatrix(double[][] mainMatrix, double[][] tableOfKsiIntegral, double[][] tableOfEtaIntegral) {
         double[][] table2DlaPc1 = new double[tableOfKsiIntegral.length][4];
         for (int i = 0; i < tableOfKsiIntegral.length; i++) {
             for (int j = 0; j < 4; j++) {
@@ -194,7 +185,7 @@ public class AppDla3pkt {
         return table2DlaPc1;
     }
 
-    private static double[][] getTableOfKsi1IntegralMultipliedByMatrix(double[][] tableOfKsiIntegral, double[][] tableOfEtaIntegral) {
+    private static double[][] getTableOfKsi1IntegralMultipliedByMatrix(double[][] mainMatrix, double[][] tableOfKsiIntegral, double[][] tableOfEtaIntegral) {
         double[][] table1DlaPc1 = new double[tableOfKsiIntegral.length][4];
         for (int i = 0; i < tableOfKsiIntegral.length; i++) {
             for (int j = 0; j < 4; j++) {
@@ -202,91 +193,5 @@ public class AppDla3pkt {
             }
         }
         return table1DlaPc1;
-    }
-
-
-
-
-
-
-    private static double[][] calculateAndShowSecondArray() {
-        //Druga tabela od gory
-        double[] ksi = new double[]{(-1 / sqrt(3)), (1 / sqrt(3)), (-1 / sqrt(3)), (1 / sqrt(3))};
-        double[][] table4 = new double[4][4];
-        List<Function<Double, Double>> functions = new ArrayList<>() {
-            {
-                add(eta -> 0.25 * (eta - 1));
-                add(eta -> 0.25 * (-eta - 1));
-                add(eta -> 0.25 * (eta + 1));
-                add(eta -> 0.25 * (1 - eta));
-            }
-        };
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                table4[i][j] = functions.get(j).apply(ksi[i]); //[i] w dol, [j] w prawo
-            }
-        }
-
-        //=======================================
-        double pcx[] = MatrixService.getPcx();
-        double sum1DxDeta = 0;
-        for (int j = 0; j < table4.length; j++) {
-            sum1DxDeta += table4[0][j] * pcx[j];
-        }
-        System.out.println("Suma1DxDeta: " + sum1DxDeta);
-        mainMatrix[1][0] = sum1DxDeta;
-        //=========================================
-        double pcy[] = MatrixService.getPcy();
-        double sum1DyDeta = 0;
-        for (int j = 0; j < table4.length; j++) {
-            sum1DyDeta += table4[0][j] * pcy[j];
-        }
-        System.out.println("Suma1DyDeta: " + sum1DyDeta);
-        mainMatrix[1][1] = sum1DyDeta;
-        //=========================================
-
-
-        System.out.println("SecondArray");
-        return table4;
-    }
-
-    private static double[][] calculateAndShowFirstArray() throws FileNotFoundException {
-        //Pierwsza tabela od gory
-        double[] eta = new double[]{(-1 / sqrt(3)), (-1 / sqrt(3)), (1 / sqrt(3)), (1 / sqrt(3))};
-        double[][] table4 = new double[4][4];
-        List<Function<Double, Double>> functions = new ArrayList<>() {
-            {
-                add(ksi -> 0.25 * (ksi - 1));
-                add(ksi -> 0.25 * (1 - ksi));
-                add(ksi -> 0.25 * (ksi + 1));
-                add(ksi -> 0.25 * (-ksi - 1));
-            }
-        };
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                table4[i][j] = functions.get(j).apply(eta[i]); //[i] w dol, [j] w prawo
-            }
-        }
-        //======================================
-        double pcx[] = MatrixService.getPcx();
-        double sum1DxDksi = 0;
-        for (int j = 0; j < table4.length; j++) {
-            sum1DxDksi += table4[0][j] * pcx[j];
-        }
-        System.out.println("Suma1DxDksi: " + sum1DxDksi);
-        mainMatrix[0][0] = sum1DxDksi;
-        //========================================
-        double pcy[] = MatrixService.getPcy();
-        double sum1DyDksi = 0;
-        for (int j = 0; j < table4.length; j++) {
-            sum1DyDksi += table4[0][j] * pcy[j];
-        }
-        System.out.println("Suma1DyDksi: " + sum1DyDksi);
-        mainMatrix[0][1] = sum1DyDksi;
-        //========================================
-
-
-        System.out.println("FirstArray");
-        return table4;
     }
 }
